f319d7cf1c0d87e6241925ad01cf66a302beaecd
brigittaforrai <forraibrigi@gmail.com>
2019-01-23 12:42:21 +0100

    build

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,6 @@
 const merge = require('babel-merge')
 
 module.exports = {
-  chainWebpack: config => {
-    config.module.rule('vue').use("vue-loader").tap(options => merge(
-      options, { transformAssetUrls: { 'video': 'src', 'source': 'src'}}
-    ))
-  },
   lintOnSave: true,
   baseUrl: '',
   outputDir: 'docs'
